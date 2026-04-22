77f6a0a1709cef71c8977b3ee9b880a4c1784dd2
Scifeks <scifeks@protonmail.com>
2026-04-22 16:52:29 -0700

    TAL-94 - Cleaning up dead code, adding todo for future me

--- web/adapters/no_approval_prompt.py
+++ web/adapters/no_approval_prompt.py
@@ -3,6 +3,8 @@
 from __future__ import annotations
 
 
+# todo: This is an Interface Segregation Principle violation.
+# this is totally irrelevant for web. This is a REPL concern.
 class NoApprovalPromptAdapter:
     """Satisfies UserPromptPort; always returns True without prompting."""
 
